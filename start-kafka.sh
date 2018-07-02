36b1263e8d944c9b336c1656def2f7347c338840
Rodrigo Martínez <dev@brunneis.com>
2018-07-02 15:03:12 +0200

    Delete start-kafka.sh

--- start-kafka.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DATA_PATH=$(cat DATA_PATH)/kafka
-
-docker run --restart unless-stopped -d --net=host  \
--v $DATA_PATH:/opt/kafka/default/logs \
---ulimit nofile=90000:90000 \
---name kafka \
-catenae/kafka
